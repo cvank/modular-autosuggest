4d10debbafda2bf42c571604faf4a2506256e9d7
Chandrashekar <chandrashekar.vankayalapati@gmail.com>
2018-01-19 17:45:40 +0530

    added modules

---
+++ refactored/refactored-fuzy-levenshtein/src/main/java/org/refactored/fuzy/levenshtein/App.java
@@ -0,0 +1,13 @@
+package org.refactored.fuzy.levenshtein;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
